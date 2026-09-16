4535ad0431316fd29918b483e695758a26b9a621
KeenanStacey05 <151028957+KeenanStacey05@users.noreply.github.com>
2026-09-16 11:00:46 +0100

    Tasky Lab 2a - Exercise done

--- tasky/src/components/Task.jsx
+++ tasky/src/components/Task.jsx
@@ -5,6 +5,7 @@ const Task = (props) => {
             <p className="title">{props.title}</p>
             <p>Due: {props.deadline}</p>
             <p>{props.description}</p>
+            <p className="priority">Priority: {props.priority}</p>
         </div>
     )
 
